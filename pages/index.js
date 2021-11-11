155a2a4e9dcc370791ea529a901cea4e021f2e1e
Alexey Gmitron <gmitron01@gmail.com>
2021-11-11 21:00:01 +0700

    feat: layout

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,26 @@
-import Test from '@components/sub1/sub2/Test'
+
+import { Navbar, Footer, Hero, Breadcrumbs } from "@components/common"
+import { CourseList } from "@components/course"
+import { OrderCard } from "@components/order"
+import { EthRates, WalletBar } from "@components/web3"
 
 export default function Home() {
   return (
-    <>
-      <Test />
-    </>
+    <div>
+      <div className="relative bg-white overflow-hidden">
+        <div className="relative max-w-7xl mx-auto px-4">
+          <Navbar />
+          <div className="fit">
+            <Hero />
+            <Breadcrumbs />
+            <WalletBar />
+            <EthRates />
+            <OrderCard />
+            <CourseList />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </div>
   )
 }
--- pages/sub1/Test.js
+++
@@ -1,2 +0,0 @@
-import Test from '@components/sub1/sub2/Test'
-
